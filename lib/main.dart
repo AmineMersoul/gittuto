63bf4e6e2b2bd45cd26e3895560b38a515322980
Amine Mersoul <amine.3dsmax@live.fr>
2018-07-20 21:20:34 +0100

    change app title

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Git Tuto Home Page'),
     );
   }
 }
