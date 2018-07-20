5630ca10e5f5c35e2471ea306c0c04ea39bd941b
Amine Mersoul <amine.3dsmax@live.fr>
2018-07-20 21:38:02 +0100

    change also the title

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Git Tuto',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
